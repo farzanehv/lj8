83b3961881c4b32eb4df60787fabd09cce379f63
Farzaneh <farzaneh.v@gmail.com>
2020-07-11 16:54:11 +0430

    adding private class product

---
+++ StreamExample.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Product{
+	
+	int id;
+	String name;
+	float price;
+
+	public Product(int inputId, String inputName, float inputPrice){
+		
+		this.id = inputId;
+		this.name = inputName;
+		this.price = inputPrice;
+	}
+}
+
