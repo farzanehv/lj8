8e32e3e8af1d76bce2cfca558e4e1c4895c45c9f
Farzaneh <farzaneh.v@gmail.com>
2020-07-11 17:17:06 +0430

    adding the main stream filter map collect

--- StreamExample.java
+++ StreamExample.java
@@ -15,3 +15,22 @@ class Product{
 	}
 }
 
+
+public class StreamExample{
+
+	public static void main(String []args){
+		List<Product> productList = new ArrayList();
+
+		productList.add(new Product(1, "a", 10000));
+		productList.add(new Product(2, "b", 20000));
+		productList.add(new Product(3, "c", 30000));
+		productList.add(new Product(4, "d", 40000));
+
+		List<Float> productPriceList = productList.stream()
+			.filter(p -> p.price > 20000)
+			.map(p -> p.price)
+			.collect(Collectors.toList());
+
+		System.out.println(productPriceList);
+	}
+}
